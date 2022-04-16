222cf264143a1e07a9d37a1a4eeaa3cbf43e63d9
Mariano Vera <marianonvera.22@gmail.com>
2022-04-16 15:59:12 -0300

    Empece acerca de mi

--- Portfolio-en-Angular/src/app/app.module.ts
+++ Portfolio-en-Angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
