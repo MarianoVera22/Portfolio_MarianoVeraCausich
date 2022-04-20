4d2d1f4febd015215b0a3c9558900b9756f1f222
Mariano Vera <marianonvera.22@gmail.com>
2022-04-19 23:08:30 -0300

    Skills logos

--- Portfolio-en-Angular/src/app/app.module.ts
+++ Portfolio-en-Angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,6 +9,7 @@ import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './experiencia/experiencia.component';
 import { EducacionComponent } from './educacion/educacion.component';
 import { AcercaDeTextoComponent } from './acerca-de-texto/acerca-de-texto.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +19,13 @@ import { AcercaDeTextoComponent } from './acerca-de-texto/acerca-de-texto.compon
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    AcercaDeTextoComponent
+    AcercaDeTextoComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
